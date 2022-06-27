f8ecf2d8ff92bcd80398bd12cc66c524b428d13a
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2022-06-27 16:05:45 -0300

    feat: height widget

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -10,11 +10,11 @@ export const Container = styled.div<Props>`
   grid-template-columns: ${props => props.open ? '240px 1fr 1fr 1fr 1fr' : '80px 1fr 1fr 1fr 1fr'};
   grid-template-rows: 60px .6fr 1.5fr 1fr 60px;
   grid-template-areas:
-  "sidebar nav nav nav nav"
-  "sidebar content1 content2 content3 content4"
-  "sidebar main main main main"
-  "sidebar list list list list"
-  "footer footer footer footer footer";
+    "sidebar nav nav nav nav"
+    "sidebar content1 content2 content3 content4"
+    "sidebar main main main main"
+    "sidebar list list list list"
+    "footer footer footer footer footer";
   grid-gap: 0.1rem;
   font-weight: 800;
   text-transform: uppercase;
@@ -22,33 +22,33 @@ export const Container = styled.div<Props>`
   color: #004d40;
   text-align: center;
 
-   @media only screen and (max-width: 1200px) {
+  @media only screen and (max-width: 1200px) {
     grid-template-columns: ${props => props.open ? '0.5fr 1fr 1fr' : '80px 1fr 1fr'};
     grid-template-rows: 60px 1fr 1fr 2fr 1fr 50px;
     grid-template-areas:
-       "sidebar nav nav"
-       "sidebar content1 content2"
-       "sidebar content3 content4"
-       "sidebar main main"
-       "sidebar list list"
-       "footer footer footer";
-   }
+      "sidebar nav nav"
+      "sidebar content1 content2"
+      "sidebar content3 content4"
+      "sidebar main main"
+      "sidebar list list"
+      "footer footer footer";
+  }
 
-   @media only screen and (max-width: 880px) {
+  @media only screen and (max-width: 880px) {
     grid-template-columns: ${props => props.open ? '0.3fr 1fr' : '80px 1fr'};
     grid-template-rows: 60px 1fr 1fr 1fr 1fr 3fr 2fr 60px;
     grid-template-areas:
-       "sidebar nav"
-       "sidebar content1"
-       "sidebar content2"
-       "sidebar content3"
-       "sidebar content4"
-       "sidebar main"
-       "sidebar list"
-       "footer footer";
-   }
+      "sidebar nav"
+      "sidebar content1"
+      "sidebar content2"
+      "sidebar content3"
+      "sidebar content4"
+      "sidebar main"
+      "sidebar list"
+      "footer footer";
+  }
 
-   @media only screen and (max-width: 550px) {
+  @media only screen and (max-width: 550px) {
     grid-template-columns: 1fr;
     grid-template-rows: .5fr 1fr 1fr 1fr 1fr 3fr 2fr .5fr;
     grid-template-areas:
@@ -60,7 +60,7 @@ export const Container = styled.div<Props>`
       "main"
       "list"
       "footer";
-   }
+  }
 `;
 
 export const Nav = styled.nav`
@@ -71,14 +71,12 @@ export const Nav = styled.nav`
   align-items: center;
   justify-content: space-between;
 `
-
 export const Main = styled.main`
   background: #84ffff;
   grid-area: main;
   border-radius: var(--main-radius);
   padding-top: var(--main-padding);
 `
-
 export const Sidebar = styled.div<Props>`
   grid-area: sidebar;
 
@@ -92,21 +90,22 @@ export const Sidebar = styled.div<Props>`
 export const ContentOne = styled.div`
   grid-area: content1;
   padding: var(--main-padding);
-  `
-
+  min-height: 120px;
+`
 export const ContentTwo = styled.div`
   grid-area: content2;
   padding: var(--main-padding);
-  `
-
+  min-height: 120px;
+`
 export const ContentThree = styled.div`
   grid-area: content3;
   padding: var(--main-padding);
-  `
-
+  min-height: 120px;
+`
 export const ContentFour = styled.div`
   grid-area: content4;
   padding: var(--main-padding);
+  min-height: 120px;
 `
 export const List = styled.div`
   background: #ff25ee;
@@ -114,7 +113,6 @@ export const List = styled.div`
   border-radius: var(--main-radius);
   padding-top: var(--main-padding);
 `
-
 export const Footer = styled.footer`
   background: #1de9b6;
   grid-area: footer;
