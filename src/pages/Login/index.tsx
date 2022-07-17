a8197141541fae927595753304fb6b731b3ab4ab
Valdir Silva <valdir.ti@gmail.com>
2022-07-17 18:32:37 -0300

    feat: create login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,23 @@
 import { useState, useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Google, Facebook, GitHub } from '@mui/icons-material'
 
 import { auth } from '../../firebase'
 import { signInWithEmailAndPassword } from "firebase/auth"
 
 import { AuthContext } from '../../context/authentication/authContext'
 
-import './styles.scss'
+import backgroundImg from '../../assets/bg-01.jpg'
+
+import * as S from './styles'
 
 const Login = () => {
 
   const [error, setError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [checked, setChecked] = useState(false)
 
   const { state: { currentUser }, dispatch } = useContext(AuthContext)
 
@@ -23,33 +28,92 @@ const Login = () => {
     setError(false)
 
     signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        dispatch({type: "LOGIN", payload: user})
-        navigate('/')
-      })
-      .catch((error) => {
-        setError(true)
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage)
-      });
+    .then((userCredential) => {
+      const user = userCredential.user;
+      dispatch({type: "LOGIN", payload: user})
+      navigate('/')
+    })
+    .catch((error) => {
+      setError(true)
+      setErrorMessage('Email e/ou Password inválidos!')
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage)
+      setTimeout(() => {
+        setError(false)
+        setErrorMessage('')
+      }, 2500)
+    });
+  }
+
+  const handleLoginGithub = () => {
+    handleFunctionality()
+    console.log('Login with Github')
+  }
+  const handleLoginGoogle = () => {
+    handleFunctionality()
+    console.log('Login with Google')
+  }
+  const handleLoginFacebook = () => {
+    handleFunctionality()
+    console.log('Login with Facebook')
+  }
+
+  const handleForgot = () => {
+    handleFunctionality()
+    console.log('handleForgot')
+  }
+
+  const handleRemember = () => {
+    handleFunctionality()
+    console.log('handle Remember')
+    setChecked(!checked)
+  }
+
+  // TODO: Remover após implementar o login com outros serviços
+  function handleFunctionality() {
+    setError(true)
+    setErrorMessage('Funcionalidade em construção!')
+    setTimeout(() => {
+      setError(false)
+      setErrorMessage('')
+    }, 2500)
   }
 
   useEffect(() => {
     currentUser && navigate('/')
-  }, [])
-
+  }, [currentUser])
 
   return (
-    <div className='login'>
-      <form onSubmit={handleLogin}>
-        <input type="email" placeholder='E-mail' onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
-        <button type='submit'>Login</button>
-        {error && <span>Wrong email/password!</span>}
-      </form>
-    </div>
+    <S.Container>
+      <S.ColumnOne>
+        <S.Background src={backgroundImg} alt="Background image" />
+      </S.ColumnOne>
+      <S.ColumnTwo>
+        <S.Title>Login to continue</S.Title>
+        <S.Form onSubmit={handleLogin}>
+          <S.Input type='email' placeholder='E-mail' onChange={(e) => setEmail(e.target.value)} />
+          <S.Input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+          <S.Actions>
+            <S.RememberLabel>
+              <S.RememberInput type="checkbox"  checked={checked} onChange={handleRemember} />
+              <S.RememberText>Remember me</S.RememberText>
+            </S.RememberLabel>
+            <S.ForgotButton onClick={handleForgot}>Forgot Password?</S.ForgotButton>
+          </S.Actions>
+          <S.Button type='submit'>Login</S.Button>
+          <S.SocialLogin>
+            <S.SocialLoginTitle>or sign up using</S.SocialLoginTitle>
+            <S.SocialLoginIcons>
+              <S.SocialIconGithub sx={{ fontSize: '1.2rem' }} onClick={handleLoginGithub}/>
+              <S.SocialIconGoogle sx={{ fontSize: '1.2rem' }} onClick={handleLoginGoogle}/>
+              <S.SocialIconFacebook sx={{ fontSize: '1.2rem' }} onClick={handleLoginFacebook}/>
+            </S.SocialLoginIcons>
+          </S.SocialLogin>
+          {error && <S.Error>{errorMessage}</S.Error>}
+        </S.Form>
+      </S.ColumnTwo>
+    </S.Container>
   )
 }
 
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,172 @@
+import styled from "styled-components";
+import { Google, Facebook, GitHub } from '@mui/icons-material'
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    height: 97vh;
+    width: 98vw;
+    font-family: 'Roboto', sans-serif;
+    @media only screen and (max-width: 550px) {
+        flex-direction: column-reverse;
+        width: 96vw;
+        height: 97vh;
+    }
+    `
+
+export const ColumnOne = styled.div`
+    height: 500px;
+    width: 350px;
+    @media only screen and (max-width: 550px) {
+        height: 250px;
+    }
+    `
+export const Background = styled.img`
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    @media only screen and (max-width: 550px) {
+        border-bottom-right-radius: 5px;
+        border-top-left-radius: 0px;
+    }
+    `
+export const Actions = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 60%;
+    margin-bottom: 5px;
+    @media only screen and (max-width: 550px) {
+        width: 75%;
+    }
+`
+export const RememberLabel = styled.label`
+    display: flex;
+`
+export const RememberInput = styled.input`
+    font-size: 10px;
+    font-weight: 500;
+    cursor: pointer;
+    padding: 0;
+    margin: 0;
+`
+export const RememberText = styled.h1`
+    font-size: 10px;
+    font-weight: 500;
+    margin-left: 5px;
+    cursor: pointer;
+`
+export const ForgotButton = styled.a`
+    font-weight: 500;
+    font-size: 10px;
+    cursor: pointer;
+`
+
+export const ColumnTwo = styled.div`
+    height: 500px;
+    width: 350px;
+    background-color: #dddddd73;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+    position: relative;
+    @media only screen and (max-width: 550px) {
+        height: 350px;
+        border-top-left-radius: 5px;
+        border-bottom-right-radius: 0px;
+    }
+    `
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 200px;
+    @media only screen and (max-width: 550px) {
+        height: 60%;
+        justify-content: space-evenly;
+    }
+`
+export const Title = styled.h1`
+    font-size: 20px;
+    font-weight: 500;
+    color: #a1a4a9;
+    @media only screen and (max-width: 550px) {
+        margin-bottom: 10px;
+    }
+`
+
+export const Input = styled.input`
+    width: 200px;
+    height: 30px;
+    margin: 10px;
+    border-radius: 5px;
+    border-width: 0.1px;
+    margin: 10px 0;
+    padding: 5px;
+    &:focus{
+        outline: none;
+    }
+    @media only screen and (max-width: 550px) {
+        width: 250px;
+    }
+`
+export const Button = styled.button`
+    background-color: purple;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    font-weight: bold;
+    width: 212px;
+    height: 35px;
+    margin: 0px;
+    border-radius: 5px;
+    border-width: 1px;
+    padding: 5px;
+    @media only screen and (max-width: 550px) {
+        width: 260px;
+        margin-bottom: 10px;
+    }
+`
+export const Error = styled.div`
+    position: absolute;
+    font-size: 12px;
+    color: red;
+    font-weight: bold;
+    bottom: 165px;
+    @media only screen and (max-width: 550px) {
+        bottom: 95px;
+    }
+`
+export const SocialLogin = styled.div`
+    margin-top: 20px;
+`
+export const SocialLoginTitle = styled.h1`
+    font-weight: 500;
+    font-size: 12px;
+    color: #a1a4a9;
+`
+export const SocialLoginIcons = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+export const SocialIconGithub = styled(GitHub)`
+    color: #a1a4a9;
+    cursor: pointer;
+`
+export const SocialIconGoogle = styled(Google)`
+    color: #a1a4a9;
+    cursor: pointer;
+`
+export const SocialIconFacebook = styled(Facebook)`
+    color: #a1a4a9;
+    cursor: pointer;
+`
