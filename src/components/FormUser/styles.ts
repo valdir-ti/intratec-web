66971b6ba74f449824b2a7c3a2ecd13da74b0901
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2022-07-26 14:40:51 -0300

    refact: form styles

--- src/components/FormUser/styles.ts
+++ src/components/FormUser/styles.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { DriveFolderUploadOutlined } from '@mui/icons-material'
 
 export const Form = styled.form`
     display: flex;
@@ -12,56 +11,3 @@ export const Title = styled.h2`
     margin-bottom: 1rem;
     margin-top: 1rem;
 `
-export const FormInputContainer = styled.div`
-    width: 45%;
-    margin: 5px;
-    @media only screen and (max-width: 880px) {
-        width: 90%;
-    }
-    @media only screen and (max-width: 280px) {
-        width: 100%;
-    }
-`
-export const FormLabel = styled.label`
-    display: flex;
-    align-items: center;
-    gap: 10px;
-`
-export const FormInput = styled.input`
-    width: 100%;
-    height: 35px;
-    padding: 5px;
-    border: none;
-    border-bottom: 1px solid ${props => props.theme.colors.text};
-    border-radius: 2px;
-    margin-top: 5px;
-`
-export const FormInputCheckboxContainer = styled.div`
-    width: 45%;
-    margin: 5px;
-    display: flex;
-    align-items: center;
-    @media only screen and (max-width: 880px) {
-        width: 90%;
-    }
-    @media only screen and (max-width: 280px) {
-        width: 100%;
-    }
-`
-export const FormInputCheckbox = styled.input`
-    margin-left: 1rem;
-`
-export const Input = styled.input``
-export const ContainerImg = styled.div`
-    padding: 1rem;
-`
-export const ContainerContentImg = styled.img`
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    object-fit: cover;
-`
-export const FormDriveFolderIcon = styled(DriveFolderUploadOutlined)`
-    cursor: pointer;
-`
-export const ImgInput = styled.img``
