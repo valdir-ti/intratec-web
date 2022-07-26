0e1fbd5301f90d462886bc274aa3ad7b56de3b94
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2022-07-26 14:20:03 -0300

    feat: real data on widgets

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,45 +1,106 @@
+import { useEffect, useState } from 'react';
 import {
   KeyboardArrowDown,
   KeyboardArrowUp,
+  PersonOutline,
+  MonetizationOn,
+  AccountBalanceWallet,
+  Inventory
 } from '@mui/icons-material';
+
+import { db } from '../../firebase';
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 import * as S from './styles'
 
-type DataType = {
-  title: string;
-  type: string;
-  isMoney: boolean;
-  amount: number;
-  link: string;
-  status: string;
-  iconColor: string;
-  fontIconColor: string;
-  icon: React.ReactNode;
-}
 interface IWidget {
-  data: DataType;
+  item: string;
 }
 
-const Widget = ({ data }: IWidget) => {
-  const { title, type, isMoney, amount, link, status, iconColor, fontIconColor, icon } = data;
+const Widget = ({ item }: IWidget) => {
+  const [total, setTotal] = useState(0)
+  let data;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const activeItems = query(
+        collection(db, item),
+        where('isActive', '==', true)
+      )
+      const Items = await getDocs(activeItems)
+      setTotal(Items.docs.length)
+    }
+    fetchData()
+  }, [item]);
+
+  switch (item) {
+    case 'users':
+      data = {
+        title: 'USERS',
+        isMoney: false,
+        link: 'See all users',
+        icon: <PersonOutline />,
+        iconColor: 'rgba(255,0,0,0.2)',
+        fontIconColor: 'crimson',
+        status: 'positive',
+      }
+    break;
+    case 'products':
+      data = {
+        title: 'PRODUCTS',
+        isMoney: false,
+        link: 'View all products',
+        icon: <Inventory />,
+        iconColor: 'rgba(218,165,32,0.2)',
+        fontIconColor: 'goldenrod',
+        status: 'negative',
+      }
+    break;
+    case 'earnings':
+      data = {
+        title: 'EARNINGS',
+        isMoney: true,
+        link: 'View net earnings',
+        icon: <MonetizationOn />,
+        iconColor: 'rgba(0,128,0,0.2)',
+        fontIconColor: 'green',
+        status: 'negative',
+      }
+    break;
+    case 'balance':
+      data = {
+        title: 'BALANCE',
+        isMoney: true,
+        link: 'See details',
+        icon: <AccountBalanceWallet />,
+        iconColor: 'rgba(128,0,128,0.2)',
+        fontIconColor: 'purple',
+        status: 'positive',
+      }
+    break;
+    default:
+      break;
+  }
+
+
   return (
     <S.WidgetContainer>
       <S.Left>
-        <S.LeftTitle>{title}</S.LeftTitle>
-        <S.LeftCounter>{isMoney && 'R$'} {amount}</S.LeftCounter>
+        <S.LeftTitle>{data?.title}</S.LeftTitle>
+        <S.LeftCounter>{data?.isMoney && 'R$'} {total}</S.LeftCounter>
         <S.LeftLink>
-          <S.LinkStyled to={type}>
-            {link}
+          <S.LinkStyled to={item}>
+            {data?.link}
           </S.LinkStyled>
         </S.LeftLink>
       </S.Left>
       <S.Right>
-        <S.RightPercent status={status}>
-          {status === 'positive' ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+        <S.RightPercent status={data?.status}>
+          {data?.status === 'positive' ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
           20%
         </S.RightPercent>
-        <S.RightIconWrapper color={iconColor} fontColor={fontIconColor}>
-          {icon}
+        <S.RightIconWrapper color={data?.iconColor} fontColor={data?.fontIconColor}>
+          {data?.icon}
         </S.RightIconWrapper>
       </S.Right>
     </S.WidgetContainer>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,6 @@
-import { useContext } from 'react';
-
-import {
-  PersonOutline,
-  ShoppingCart,
-  MonetizationOn,
-  AccountBalanceWallet
-} from '@mui/icons-material';
-
-import Widget from '../../components/Widget';
+import Layout from '../Layout';
 import Chart from '../../components/Chart';
+import Widget from '../../components/Widget';
 import Feature from '../../components/Feature';
 import TableList from '../../components/Table';
 
@@ -23,71 +15,22 @@ import {
   List as ListStyled,
 } from './styles'
 
-import Layout from '../Layout';
-
 const Home = () => {
-
-  const dataWidget = [
-    {
-      type: 'users',
-      title: 'USERS',
-      isMoney: false,
-      amount: 135,
-      link: 'See all users',
-      icon: <PersonOutline />,
-      iconColor: 'rgba(255,0,0,0.2)',
-      fontIconColor: 'crimson',
-      status: 'positive',
-    },
-    {
-      type: 'orders',
-      title: 'ORDERS',
-      amount: 14,
-      isMoney: false,
-      link: 'View all orders',
-      icon: <ShoppingCart />,
-      iconColor: 'rgba(218,165,32,0.2)',
-      fontIconColor: 'goldenrod',
-      status: 'negative',
-    },
-    {
-      type: 'earnings',
-      title: 'EARNINGS',
-      amount: 56,
-      isMoney: true,
-      link: 'View net earnings',
-      icon: <MonetizationOn />,
-      iconColor: 'rgba(0,128,0,0.2)',
-      fontIconColor: 'green',
-      status: 'negative',
-    },
-    {
-      type: 'balance',
-      title: 'BALANCE',
-      amount: 156,
-      isMoney: true,
-      link: 'See details',
-      icon: <AccountBalanceWallet />,
-      iconColor: 'rgba(128,0,128,0.2)',
-      fontIconColor: 'purple',
-      status: 'positive',
-    },
-  ]
 
   return (
     <Layout>
       <Container>
           <ContentOne>
-            <Widget data={dataWidget[0]}/>
+            <Widget item='users'/>
           </ContentOne>
           <ContentTwo>
-            <Widget data={dataWidget[1]}/>
+            <Widget item='products' />
           </ContentTwo>
           <ContentThree>
-            <Widget data={dataWidget[2]}/>
+            <Widget item='earnings' />
           </ContentThree>
           <ContentFour>
-            <Widget data={dataWidget[3]}/>
+            <Widget item='balance' />
           </ContentFour>
            <FeatureStyled>
             <Feature />
