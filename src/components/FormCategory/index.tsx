711e00c0788fe037a778e2582e2feee2d7b6ed1e
Valdir Silva <valdir.ti@gmail.com>
2023-01-18 23:05:50 -0300

    feat: update caetgories form

--- src/components/FormCategory/index.tsx
+++ src/components/FormCategory/index.tsx
@@ -1,186 +1,199 @@
-import { FormEvent, useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid'
+import { FormEvent, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
-import { db } from '../../firebase';
-import { doc, serverTimestamp, setDoc, updateDoc } from 'firebase/firestore'
+import Toaster from "../Toaster";
+import FormField from "../Form/FormField";
+import FormButton from "../Form/FormButton";
+import Layout from "../../pages/Layout";
+import FormCheckbox from "../Form/FormCheckbox";
 
-import Toaster from '../Toaster';
-import FormField from '../Form/FormField';
-import FormButton from '../Form/FormButton';
-import Layout from '../../pages/Layout';
-import FormCheckbox from '../Form/FormCheckbox';
-
-import * as S from './styles';
-import { supabaseClient } from '../../supabase';
-import FormSelect from '../Form/FormSelect';
+import * as S from "./styles";
+import { supabaseClient } from "../../supabase";
+import FormSelect from "../Form/FormSelect";
 
 interface LocationProps {
-    id: string;
-    title: string;
-    parent_id: string;
-    status: boolean;
+  id: string;
+  title: string;
+  parent_id: string;
+  status: boolean;
 }
 
 interface Props {
-    isEditing?: boolean;
+  isEditing?: boolean;
 }
 
-const FormProduct = ({ isEditing }: Props) => {
+const FormCategory = ({ isEditing }: Props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const state = location.state as LocationProps;
+  const currentUser = JSON.parse(localStorage.getItem("user")!);
 
-    const navigate = useNavigate()
-    const location = useLocation()
-    const state = location.state as LocationProps
-    const currentUser = JSON.parse(localStorage.getItem("user")!)
+  const [title, setTitle] = useState("");
+  const [status, setStatus] = useState(false);
+  const [parentId, setParentId] = useState("");
+  const [id, setId] = useState<string>(state?.id || "");
+  const [categories, setCategories] = useState<any>();
 
-    const [title, setTitle] = useState("");
-    const [status, setStatus] = useState(false);
-    const [parentId, setParentId] = useState("");
-    const [id, setId] = useState<string>(state?.id || "");
-    const [categories, setCategories] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState<boolean>(false);
+  const [toasterMessage, setToasterMessage] = useState<string>("");
+  const [toasterSeverity, setToasterSeverity] = useState<string>("");
 
-    const [loading, setLoading] = useState(false)
-    const [open, setOpen] = useState<boolean>(false)
-    const [toasterMessage, setToasterMessage] = useState<string>("")
-    const [toasterSeverity, setToasterSeverity] = useState<string>("")
+  async function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+    setLoading(true);
 
-    async function handleFormSubmit(e: FormEvent) {
-        e.preventDefault();
-        setLoading(true)
-
-        if(!title) {
-            toasterStart("error", "O título da categoria é obrigatório (*)")
-            setLoading(false)
-            return
-        }
-
-        try {
-            const itemId = uuidv4()
-            const { error } = await supabaseClient.from('categories').insert([
-                { id: itemId, title: title, status: status, user_id: currentUser.uid, parent_id: parentId },
-            ])
-
-            if(!error){
-                toasterStart("success", "Categoria cadastrada com sucesso!")
-                setTimeout(() => {
-                    navigate('/categories')
-                }, 1500)
-            }
-
-        } catch (err) {
-            console.log('Error: ', err)
-        }
+    if (!title) {
+      toasterStart("error", "O título da categoria é obrigatório (*)");
+      setLoading(false);
+      return;
     }
 
-    async function handleFormUpdate(e: FormEvent) {
-        e.preventDefault();
-        setLoading(true)
+    try {
+      const itemId = uuidv4();
+      const { error } = await supabaseClient.from("categories").insert([
+        {
+          id: itemId,
+          title: title,
+          status: status,
+          user_id: currentUser.uid,
+          parent_id: parentId,
+        },
+      ]);
 
-        if(!title) {
-            toasterStart("error", "O título da categoria é obrigatório (*)")
-            setLoading(false)
-            return
-        }
+      if (!error) {
+        toasterStart("success", "Categoria cadastrada com sucesso!");
+        setTimeout(() => {
+          navigate("/categories");
+        }, 1500);
+      }
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  }
 
-        try {
-            const { error } = await supabaseClient.from('categories')
-            .update({ title: title, status: status, user_id: currentUser.uid, parent_id: parentId, updated_at: new Date() })
-            .eq('id', id)
+  async function handleFormUpdate(e: FormEvent) {
+    e.preventDefault();
+    setLoading(true);
 
-            if(!error){
-                toasterStart("success", "Categoria atualizada com sucesso!")
-                setTimeout(() => {
-                    navigate('/categories')
-                }, 1500)
-            }
-
-        } catch (err) {
-            console.log('Error: ', err)
-        }
-
+    if (!title) {
+      toasterStart("error", "O título da categoria é obrigatório (*)");
+      setLoading(false);
+      return;
     }
 
-    function toasterStart(severity: string, message: string){
-        setToasterSeverity(severity)
-        setToasterMessage(message)
-        setOpen(true)
-    }
+    try {
+      const { error } = await supabaseClient
+        .from("categories")
+        .update({
+          title: title,
+          status: status,
+          user_id: currentUser.uid,
+          parent_id: parentId,
+          updated_at: new Date(),
+        })
+        .eq("id", id);
 
-    function handleClose (event?: React.SyntheticEvent | Event, reason?: string) {
-        if (reason === 'clickaway') {
-          return;
-        }
-        setOpen(false);
+      if (!error) {
+        toasterStart("success", "Categoria atualizada com sucesso!");
+        setTimeout(() => {
+          navigate("/categories");
+        }, 1500);
+      }
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  }
+
+  function toasterStart(severity: string, message: string) {
+    setToasterSeverity(severity);
+    setToasterMessage(message);
+    setOpen(true);
+  }
+
+  function handleClose(event?: React.SyntheticEvent | Event, reason?: string) {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  }
+
+  useEffect(() => {
+    if (state) {
+      const { id, title, status, parent_id } = state;
+      setId(id);
+      setTitle(title);
+      setStatus(status);
+      setParentId(parent_id);
+    }
+  }, [state]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const { data, status } = await supabaseClient
+        .from("categories")
+        .select("id, status, title, parent_id");
+
+      if (data && status === 200) {
+        setCategories(data);
+      }
     };
+    fetchCategories();
+  }, [id]);
 
-    useEffect(() => {
-        if(state) {
-            const { id, title, status, parent_id } = state
-            setId(id)
-            setTitle(title)
-            setStatus(status)
-            setParentId(parent_id)
-        }
-    }, [state])
+  const disabledButton =
+    title === state?.title &&
+    status === state?.status &&
+    parentId === state?.parent_id;
 
-    useEffect(() => {
-        const fetchCategories = async () => {
-            const { data, status } = await supabaseClient
-                .from('categories')
-                .select("id, status, title, parent_id")
-            if(status === 200 && data?.length){
-              setCategories(data)
-            }
-          }
-          fetchCategories()
-    }, [id])
+  return (
+    <Layout>
+      <S.Form onSubmit={isEditing ? handleFormUpdate : handleFormSubmit}>
+        <S.Title>{isEditing ? "Edit Category" : "Add New Category"}</S.Title>
 
-    const disabledButton = title === state?.title && status === state?.status && parentId === state?.parent_id
+        <FormField
+          label="Title"
+          obrigatory
+          type="text"
+          placeholder="Title"
+          value={title}
+          autoComplete="off"
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-    return (
-        <Layout>
-            <S.Form onSubmit={isEditing ? handleFormUpdate : handleFormSubmit}>
-                <S.Title>{isEditing ? 'Edit Category' : 'Add New Category'}</S.Title>
+        {categories?.length ? (
+          <FormSelect
+            label="Parent Category"
+            data={categories}
+            selected={parentId}
+            onChange={(e) => setParentId(e.target.value)}
+          />
+        ) : (
+          <></>
+        )}
 
-                <FormField
-                    label='Title'
-                    obrigatory
-                    type='text'
-                    placeholder='Title'
-                    value={title}
-                    autoComplete='off'
-                    onChange={(e) => setTitle(e.target.value)}
-                />
+        <FormCheckbox
+          label="isActive"
+          checked={status}
+          type="checkbox"
+          onChange={() => setStatus(!status)}
+        />
 
-                {categories.length ? <FormSelect
-                    label='Parent Category'
-                    data={categories}
-                    selected={parentId}
-                    onChange={(e) => setParentId(e.target.value)}
-                /> : <></>}
+        <FormButton
+          label={isEditing ? "Edit" : "Save"}
+          loading={loading}
+          disabled={disabledButton}
+        />
+      </S.Form>
+      <Toaster
+        open={open}
+        title={toasterMessage}
+        severity={toasterSeverity}
+        onClose={handleClose}
+      />
+    </Layout>
+  );
+};
 
-                <FormCheckbox
-                    label='isActive'
-                    checked={status}
-                    type='checkbox'
-                    onChange={() => setStatus(!status)}
-                />
-
-                <FormButton
-                    label={isEditing ? 'Edit' : 'Save'}
-                    loading={loading}
-                    disabled={disabledButton}
-                />
-
-            </S.Form>
-            <Toaster
-                open={open}
-                title={toasterMessage}
-                severity={toasterSeverity}
-                onClose={handleClose}
-            />
-        </Layout>
-    )
-}
-
-export default FormProduct
+export default FormCategory;
